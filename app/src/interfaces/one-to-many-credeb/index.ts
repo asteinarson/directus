6c15807a9b02bdd1beb6ad77f0071f95fea68a3f
Arne Steinarson <asteinarson@gmail.com>
2021-03-13 18:05:19 +0100

    Initial adding, cred-deb validation

---
+++ app/src/interfaces/one-to-many-credeb/index.ts
@@ -0,0 +1,16 @@
+import { defineInterface } from '../define';
+import InterfaceOneToMany from '../one-to-many/one-to-many.vue';
+import Options from '../one-to-many/options.vue';
+
+export default defineInterface(({ i18n }) => ({
+	id: 'one-to-many-credeb',
+	name: 'One to many - credit-debit',
+	description: 'With sum validation on sum of credit and debit columns',
+	icon: 'arrow_right_alt',
+	component: InterfaceOneToMany,
+	types: ['alias'],
+	groups: ['o2m'],
+	relational: true,
+	options: Options,
+	recommendedDisplays: ['related-values'],
+}));
