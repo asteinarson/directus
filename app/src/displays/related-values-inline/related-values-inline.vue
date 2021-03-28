e3f602fbe0f8329f8d98660e1ad941f3781666ae
Arne Steinarson <asteinarson@gmail.com>
2021-03-28 14:45:50 +0200

    Polishing

--- app/src/displays/related-values-inline/related-values-inline.vue
+++ app/src/displays/related-values-inline/related-values-inline.vue
@@ -1,21 +1,8 @@
 <template>
 	<value-null v-if="!relatedCollection" />
-	<v-menu
-		v-else-if="['o2m', 'm2m', 'm2a', 'translations'].includes(type.toLowerCase())"
-		show-arrow
-		:disabled="value.length === 0"
-	>
-		<template #activator="{}">
-			<div>
-				<span>{{ formatItem(value) }}</span>
-				<!-- <span v-for="item in value" :key="item[primaryKeyField]">
-				</span> -->
-			</div>
-			<!-- <span @click.stop="toggle" class="toggle" :class="{ subdued: value.length === 0 }">
-				<span class="label">{{ $tc('item_count', value.length) }}</span>
-			</span> -->
-		</template>
-	</v-menu>
+	<span v-else-if="['o2m', 'm2m', 'm2a', 'translations'].includes(type.toLowerCase())">
+		{{ formatItem(value) }}
+	</span>
 	<render-template v-else :template="_template" :item="value" :collection="relatedCollection" />
 </template>
 
@@ -84,51 +71,7 @@ export default defineComponent({
 			return sa.join(', ');
 		};
 
-		return { relatedCollection, primaryKeyField, getLinkForItem, _template, format, formatItem };
-
-		function getLinkForItem(item: any) {
-			if (!relatedCollection.value || !primaryKeyField.value) return null;
-			const primaryKey = item[primaryKeyField.value.field];
-
-			return `/collections/${relatedCollection.value}/${primaryKey}`;
-		}
+		return { relatedCollection, primaryKeyField, _template, formatItem };
 	},
 });
 </script>
-
-<style lang="scss" scoped>
-.toggle {
-	position: relative;
-
-	&::before {
-		position: absolute;
-		top: -6px;
-		left: -6px;
-		z-index: 1;
-		width: calc(100% + 12px);
-		height: calc(100% + 12px);
-		background-color: var(--background-normal);
-		border-radius: var(--border-radius);
-		opacity: 0;
-		transition: opacity var(--fast) var(--transition);
-		content: '';
-	}
-
-	.label {
-		position: relative;
-		z-index: 2;
-	}
-
-	&:not(.subdued):hover::before {
-		opacity: 1;
-	}
-
-	&:not(.subdued):active::before {
-		background-color: var(--background-normal-alt);
-	}
-}
-
-.subdued {
-	color: var(--foreground-subdued);
-}
-</style>
